72b22afcec2a21a57e9003ec0453fbeba7bd7662
Alexandra Volentir <67002616+AlexandraVolentir@users.noreply.github.com>
2022-11-18 08:04:47 +0200

    .

--- HW6/main.py
+++ HW6/main.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     print(exercise1("keep it super simple"))
     print(exercise2(r"\w+", "text, ora a sosit", 3))
     print(exercise3("keep it super simple", [r"\w+", r"simple"]))
-    print(exercise4({"class": "url", "name": "url-form", "data-id": "item"}))
-    print(exercise5({"class": "url", "name": "url-form", "data-id": "item"}))
+    print(exercise4("file.xml", {"class": "url", "name": "url-form", "data-id": "item"}))
+    print(exercise5("file.xml", {"class": "url", "name": "url-form", "data-id": "item"}))
